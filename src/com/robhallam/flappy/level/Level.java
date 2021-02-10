202271f0fc3a4ca6d1b8135f82ccc7449127135a
Robert Hallam <roboffice100@gmail.com>
2021-02-10 23:54:00 +0000

    Optimised pipe regeneration and parallax

--- src/com/robhallam/flappy/level/Level.java
+++ src/com/robhallam/flappy/level/Level.java
@@ -57,24 +57,26 @@ public class Level {
 		Pipe.create();
 		for (int i = 0; i < 5 * 2; i+= 2) {
 			pipes[i] = new Pipe(OFFSET + index * 3.0f, random.nextFloat() * 4.0f);
-			pipes[i+1] = new Pipe(pipes[i].getX(), pipes[i].getY() - 12.0f); // getX so aligned horizontally
+			pipes[i+1] = new Pipe(pipes[i].getX(), pipes[i].getY() - 11.0f); // getX so aligned horizontally
 			index += 2;
 		}
 	}
 	private void updatePipes() {
-//		pipes[]
+		pipes[index % 10] = new Pipe(OFFSET + index * 3.0f, random.nextFloat() * 4.0f);
+		pipes[(index + 1) % 10] = new Pipe(pipes[index % 10].getX(), pipes[index % 10].getY() - 11.5f);
+		index += 2; // Essentially telling us how far along we are
 	}
 	
 	public void update() {
 		xScroll--; // -ve because moving map left
 		if (-xScroll % 335 == 0) map++;
-		
+		if (-xScroll > 250 && -xScroll % 120 == 0) updatePipes();
 		bird.update();
 	}
 	
 	private void renderPipes() {
 		Shader.PIPE.enable();
-		Shader.PIPE.setUniformMat4f("vw_matrix", Matrix4f.translate(new Vector3f(xScroll * 0.03f, 0.0f, 0.0f)));
+		Shader.PIPE.setUniformMat4f("vw_matrix", Matrix4f.translate(new Vector3f(xScroll * 0.05f, 0.0f, 0.0f)));
 		Pipe.getTexture().bind();
 		Pipe.getMesh().bind(); // Not calling render(), instead calling bind() and draw() as with BG
 		
